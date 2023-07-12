1c5e3e2bf34a11f6be3cbca9dd6f52ce26da5349
kcespedes.sybven <krisangela.cespedes@sybven.com>
2023-07-12 18:08:56 -0400

    Se agrega archivo para configurar envio de correos

---
+++ sendEmail.js
@@ -0,0 +1,34 @@
+import { createTransport } from 'nodemailer';
+function enviarCorreo() {
+// Configuración del transporte de correo
+const transporter = createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'krisangelacespedes@gmail.com',
+    pass: 'erfmypbnloivalyo',
+  },
+});
+
+const mailOptions = {
+    from: 'krisangelacespedes@gmail.com',
+    to: 'krisangelabackup@gmail.com',
+    subject: 'Asunto del correo electrónico',
+    text: 'Contenido del correo electrónico',
+  };
+  
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log('Error al enviar el correo electrónico:', error);
+    } else {
+      console.log('Correo electrónico enviado:', info.response);
+    }
+  });
+  
+  /*
+  Servidor SMTP: smtp.gmail.com
+Usuario SMTP: Tu dirección completa de Gmail, como tuemail@gmail.com
+Contraseña SMTP: Tu contraseña de Gmail.
+Puerto SMTP: 465 (SSL)/587 (TLS)
+TLS/SSL: Requerido.
+  */
+}
